f36115c9d223f8bc962bb1fd840a732c8d6f9cfb
SteyMuletti <98870356+SteyMuletti@users.noreply.github.com>
2023-04-28 02:17:05 -0500

    Update Subnetting Calculator.py

--- Subnetting Calculator.py
+++ Subnetting Calculator.py
@@ -7,16 +7,26 @@ def subnet_calc():
 
     # Convert the IP address and subnet mask to IPNetwork objects.
     ip_network = ipaddress.IPv4Network(f"{ip_address_str}/{subnet_mask_str}", strict=False)
-    network_address = ip_network.network_address
-    broadcast_address = ip_network.broadcast_address
+    network_address = str(ip_network.network_address)
+    broadcast_address = str(ip_network.broadcast_address)
     num_hosts = ip_network.num_addresses - 2
     num_subnets = 2 ** (32 - ip_network.prefixlen)
 
+    # Compute the network range in CIDR notation
+    cidr_notation = f"{network_address}/{ip_network.prefixlen}"
+
+    # Create a dictionary to store the results
+    results = {
+        "Network address": network_address,
+        "Broadcast address": broadcast_address,
+        "Number of hosts": num_hosts,
+        "Number of subnets": num_subnets,
+        "CIDR notation": cidr_notation,
+    }
+
     # Print out the results.
-    print(f"Network address: {network_address}")
-    print(f"Broadcast address: {broadcast_address}")
-    print(f"Number of hosts: {num_hosts}")
-    print(f"Number of subnets: {num_subnets}")
+    for key, value in results.items():
+        print(f"{key}: {value}")
 
 if __name__ == "__main__":
     subnet_calc()
